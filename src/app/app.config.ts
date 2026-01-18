02238f8067e2171942b5f717ff377279086e4e4f
Eliab Parra <eliabparra@gmail.com>
2026-01-18 01:09:36 -0400

    feat: enhance home page with login form and validation, update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api/api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Imports de Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Indica que no necesita un NgModule
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private api: ApiService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  loginUser() {
+    if (this.loginForm.valid) {
+      console.log('Datos para el backend en Render:', this.loginForm.value);
+      const { email, password } = this.loginForm.value;
+      this.api.login(email, password).then((res) => {
+        console.log('Login exitoso:', res);
+      });
+    }
+  }
 }
